e5dd9c8597151e7442a100d823ffec1e50f5274c
Boris Filipov <bfilipov@gmail.com>
2017-01-03 15:42:02 +0100

    add Event model with spec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103134714) do
+ActiveRecord::Schema.define(version: 20170103143405) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "title"
+    t.date     "date"
+    t.string   "place"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
