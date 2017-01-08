8bee99286a92e720cef8ea10d0df7e7cdbbca959
Louis Brutton <louisbrutton@gmail.com>
2017-01-08 13:26:07 +0100

    add foundation (but classes not working), add datepicker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170103143405) do
+ActiveRecord::Schema.define(version: 20170107180938) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20170103143405) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string   "title"
+    t.text     "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
