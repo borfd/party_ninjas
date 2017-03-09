aca7dcc6ab2948efad729803ae0c4833b5338b0e
Boris Filipov <bfilipov@gmail.com>
2017-03-09 23:39:57 +0100

    Add name to users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,17 @@ require 'spec_helper'
 RSpec.describe User do
   subject { User.create(email: "boris@gmail.com", password: 'yeahbro') }
   let(:event) { Event.create!(place: 'Sisy', date: Date.today, user: subject, title: "opening") }
+
   it "has can_attend? method" do
     expect(subject.can_attend?(event)).to be true
     subject.attend(event)
     expect(subject.can_attend?(event)).to be false
+  end
+
+  it "has name" do
+    subject.name = "Boris"
+    subject.save
+    subject.reload
+    expect(subject.name).to eq("Boris")
   end
 end
