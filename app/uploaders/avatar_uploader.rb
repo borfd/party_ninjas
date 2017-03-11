8b2b124c178c15b15ffc00d8daa9a17dc0e95d19
Boris Filipov <bfilipov@gmail.com>
2017-03-11 18:53:02 +0100

    Edit profile without password, resize pics

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [100, 100]
+  process resize_to_fit: [160, 160]
   process :round
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: "home#index"
   get 'home/index'
+
+  devise_for :users, controllers: { registrations: 'registrations' }
+
   resources :events do
     get :attend
     resources :comments
@@ -7,9 +11,4 @@ Rails.application.routes.draw do
       get 'filter/:filter' => :filter, as: :filter
     end
   end
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
-
 end
