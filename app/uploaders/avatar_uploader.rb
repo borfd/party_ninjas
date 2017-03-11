0f9e8288473fa9caa964ae130f41d30ac16eedef
Boris Filipov <bfilipov@gmail.com>
2017-03-11 19:04:54 +0100

    Resize_to_limit

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_fit: [160, 160]
+  process resize_to_limit: [160, 160]
   process :round
 end
