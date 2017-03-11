d1c8ea07b81ea8eb22639437a1946d5cc431f196
Boris Filipov <bfilipov@gmail.com>
2017-03-11 18:38:21 +0100

    Show registered ninjas with picture

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,28 @@
 # config/initializers/carrierwave.rb
 
 require 'mini_magick'
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+if Rails.env.test? or Rails.env.cucumber?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
+else
+  CarrierWave.configure do |config|
+    config.storage = :fog
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['S3_KEY'],
+        :aws_secret_access_key  => ENV['S3_SECRET'],
+        :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.fog_directory  = ENV['S3_BUCKET']
+  end
+end
+
 
 module CarrierWave
   module MiniMagick
@@ -46,3 +68,5 @@ module CarrierWave
     end
   end
 end
+
+
