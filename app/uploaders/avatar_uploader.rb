349b3038d2e03d4567c7581ae09232c8287158a5
Boris Filipov <bfilipov@gmail.com>
2017-03-11 19:21:00 +0100

    Per-user uploads

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,10 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_limit: [200, 200]
+  process resize_to_fill: [200, 200]
   process :round
+
+  def store_dir
+    "uploads/#{Rails.env}/avatars/#{model.id}"
+  end
 end
