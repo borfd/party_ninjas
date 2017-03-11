36b1ac5c7541db4aced5fa9e63c1ff9455fb906c
Boris Filipov <bfilipov@gmail.com>
2017-03-11 19:11:05 +0100

    Cancel account

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,6 +1,6 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
-  process resize_to_limit: [160, 160]
+  process resize_to_limit: [200, 200]
   process :round
 end
