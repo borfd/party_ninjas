e46e21e42d3fa6cc848b2ff30356cd87c2e119f0
Boris Filipov <bfilipov@gmail.com>
2017-03-13 18:54:53 +0100

    Fix exif image rotation

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,10 +1,17 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
+  process :fix_exif_rotation
   process resize_to_fill: [200, 200]
   process :round
 
   def store_dir
     "uploads/#{Rails.env}/avatars/#{model.id}"
+  end
+
+  def fix_exif_rotation #this is my attempted solution
+    manipulate! do |img|
+      img.tap(&:auto_orient)
+    end
   end
 end
