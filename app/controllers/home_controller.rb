2f736331f6eaa7e1a6a4cf07e9c38df86a2f2ed5
Boris Filipov <bfilipov@gmail.com>
2017-03-15 22:07:49 +0100

    ZEINABAR

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
-    redirect_to events_path
+    #redirect_to events_path
+    @all_events = Event.order(:date)
+    @featured_event = Event.where("date >= ?", Date.today).first
   end
+
 end
