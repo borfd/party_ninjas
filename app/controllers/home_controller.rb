61e0fad9ae7cfa679f6c0c41cca3c2ca3300f7d5
Boris Filipov <bfilipov@gmail.com>
2017-03-23 20:09:45 +0100

    Featured event is the next one

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     #redirect_to events_path
     @all_events = Event.order(:date)
-    @featured_event = Event.where("date >= ?", Date.today).first
+    @featured_event = Event.where("date >= ?", Date.today).order(:date).first
   end
 
 end
