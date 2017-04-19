06f8df69685ba3b778fd8faa6e51057f485dbb6e
Boris Filipov <bfilipov@gmail.com>
2017-04-19 09:08:44 +0200

    Do not 'fix' exif image rotation

--- app/uploaders/header_image_uploader.rb
+++ app/uploaders/header_image_uploader.rb
@@ -1,6 +1,5 @@
 class HeaderImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process :fix_exif_rotation
 
   def store_dir
     "uploads/#{Rails.env}/avatars/#{model.id}"
