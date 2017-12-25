1bcb1cb8c1c6e561df250440885cab21033ddfd1
Boris Filipov <bfilipov@gmail.com>
2017-12-25 15:55:18 +0200

    Update README, update home_spec formatting

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A webapp for all the party ninjas. Uses Ruby on Rails, Sidekiq, Postgres and Redis.
 
+Currently it is hosted on Pivotal Web Services, product decisions happen on PivotalTracker https://www.pivotaltracker.com/n/projects/1991487
+
 Run using normal rails s (will use Puma).
 
 Run a worker process with `COUNT=1 rake resque:workers`
+
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,21 +1,23 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.feature "landing page" do
-	scenario "I expect to see Party Niggaz" do
-		visit root_url
-		expect(page).to have_title("Party Niggaz")
-	end
+require 'rails_helper'
 
-  scenario "i want to register with name" do
+RSpec.feature 'landing page' do
+  scenario 'I expect to see Party Niggaz' do
     visit root_url
-    within ".user-actions" do
-        click_link "Login"
+    expect(page).to have_title('Party Niggaz')
+  end
+
+  scenario 'i want to register with name' do
+    visit root_url
+    within '.user-actions' do
+      click_link 'Login'
     end
-    click_link "Sign up"
-    fill_in "Name", with: "boris"
-    fill_in "Email", with: "boris@partyniggaz.com"
-    fill_in "Password", with: "i l1k3 p3!"
-    fill_in "Password confirmation", with: "i l1k3 p3!"
-    click_button "Sign up"
+    click_link 'Sign up'
+    fill_in 'Name', with: 'boris'
+    fill_in 'Email', with: 'boris@partyniggaz.com'
+    fill_in 'Password', with: 'i l1k3 p3!'
+    fill_in 'Password confirmation', with: 'i l1k3 p3!'
+    click_button 'Sign up'
   end
 end
