c29111170bc62000f8bff045027548a15cd3ebe1
Boris Filipov <bfilipov@gmail.com>
2017-12-25 17:00:33 +0200

    Code format

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  root to: "home#index"
+  root to: 'home#index'
 
   mount ActionCable.server => '/cable'
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users, controllers: {registrations: 'registrations'}
 
   get 'home/index'
-  delete 'users/:id', :to => 'users#destroy' , :as => :users
+  delete 'users/:id', to: 'users#destroy', as: :users
 
   post 'messages', to: 'messages#create'
 
