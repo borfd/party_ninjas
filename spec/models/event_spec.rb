68a38b43260cf8a7b7c61fd085ba93d62632a89e
Boris Filipov <bfilipov@gmail.com>
2017-12-25 15:52:10 +0200

    Rubocop format

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,38 +1,41 @@
-require "rails_helper"
+# frozen_string_literal: true
+
+require 'rails_helper'
 
 RSpec.describe Event do
-	let(:user) { User.create(email: "something", password: "something") }
-  let(:label) { Label.create(name: "sailing")}
+  let(:user) { User.create(email: 'something', password: 'something') }
+  let(:label) { Label.create(name: 'sailing') }
 
-  subject {
+  subject do
     described_class.new(
-      title:"test",
-      date:"01.01.17",
-      place:"Kater",
+      title: 'test',
+      date: '01.01.17',
+      place: 'Kater',
       user: user,
-      date: Date.yesterday)
-  }
+      date: Date.yesterday
+    )
+  end
 
-	it "is not valid without all attributes" do
-		expect(Event.new(user: user)).to_not be_valid
-	end
+  it 'is not valid without all attributes' do
+    expect(Event.new(user: user)).to_not be_valid
+  end
 
-	it "is valid with all attributes" do
-		expect(subject).to be_valid
-	end
+  it 'is valid with all attributes' do
+    expect(subject).to be_valid
+  end
 
-  it "has expired? method" do
+  it 'has expired? method' do
     expect(subject).to be_expired
     subject.date = Date.tomorrow
     expect(subject).to_not be_expired
   end
 
-  it "has header image" do
-    subject.header_image = File.open(File.absolute_path(File.join(Rails.root, "app", "assets", "images", "fusion.png")))
+  it 'has header image' do
+    subject.header_image = File.open(File.absolute_path(File.join(Rails.root, 'app', 'assets', 'images', 'fusion.png')))
     expect { subject.save }.to_not raise_error
   end
 
-  it "has labels" do
+  it 'has labels' do
     expect(subject.labels).to_not include label
     subject.labels << label
     expect(subject.labels).to include label
